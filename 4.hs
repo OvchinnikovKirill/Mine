fc5e052241752c5ab04cfdb2bd842f7f94f76cbc
Kirill Ovchinnikov <xedin666@gmail.com>
2013-04-20 18:33:58 +0400

    Kvado

---
+++ 4.hs
@@ -0,0 +1,152 @@
+import System.Random
+import System.IO
+import System.Environment	
+import Prelude
+import Kvad
+import Text.ParserCombinators.Parsec
+import Data.List.Split
+--import Monstupar
+
+--type Parser a = String->a
+--data Token = String
+
+--float :: Parser Double
+--float = do
+		--let a = read a :: Float
+		--return a
+		
+--koord :: Parser Token
+--koord = do
+		--a<-float
+		--space
+		--b<-float
+		--space
+		--c<-String
+		--return(Kord a b c)
+		
+makeKord ::  Kord
+makeKord = (Kord (read "0.12131" :: Float) (read "123123.1231" :: Float) "Gorog 1123")
+
+funct ::IO Float
+funct = getStdRandom (randomR (-100.0,100.0))
+
+--funct1 :: Koord -> Kord
+--funct1 (a++" "++b++" "++c) = Kord ((a::Float) (b::Float) c)
+--funct1 (a,b,c) = (Kord a b c) 
+ 
+
+
+setgor 101 outh = hPutStrLn outh ("0.0"++" "++"0.0"++" Gorod"++"101")
+setgor i outh = do 
+		xs <- funct
+		x1 <- funct
+		let res = show xs
+		let res1 = show x1
+		let ii = show i
+		hPutStrLn outh (res++" "++res1++" Gorod"++ii)
+		setgor (i+1) outh
+		
+--infas "0.0 0.0 Gorod101" a = (iKD (Leaf []) (Kord 0.0 0.0 "Gorod101"))
+--infas x b = do
+		--let a = (Leaf [])
+		--let a = iKD (b) (Kord (read x :: Float) (read x :: Float) (read x :: String))
+		--return a
+
+--infa :: IO () -> KD a -> Int -> KD a
+--readThat (absc,ord,name) = Kord (read absc :: Float) 
+                                  --    (read ord :: Float)
+									--  (read name :: String)
+
+  
+--kordM ::[((Float, Float), String)] -> Kord
+--kordM [((a,b),c)] = Kord a b c
+--char2float :: Char -> Float
+--char2float n = fromInteger (read [n])
+--parseKord :: String -> Parse Token
+--parseKord x2 = do
+		--a<-parsefloat x2
+		--char2float a
+		--space
+		--b<-parsefloat x2
+		--char2float b
+		--space
+		--c<-parsestring x2
+		--return (Kord (read a::Float) (read b::Float) c)
+
+--parsestring x2= do
+		--c<-letters
+		--cs<-many letters
+		--return (c:cs)
+--parsefloat x2= do
+		--c<-numbers
+		--cs<- many numbers
+		--return (c:cs)
+		
+--letters = oneOf(['a'..'z'] ++ ['A'..'Z']++['0'..'9'])
+--numbers = oneOf ['0'..'9'] ++ ['.']
+
+listToKord [] = (Kord 9999.9999 9999.9999 "ololo")
+listToKord (a:b:c:[]) = (Kord (read a::Float) (read b::Float) c)
+
+infa (x:xs) a 2 = (iKD (a) (Kord 0.0 0.0 "Gorod101"))
+infa [] a i = a		
+infa (x:xs) a i = do
+		let x2 = x
+		let e = splitOn " " x2
+		let f = listToKord e
+		--let f = parseKord x2
+		let b = (iKD (a) (f))
+		infa xs b (i+1)
+
+
+printKord [] = putStrLn (" Leaf")
+printKord ((Kord a b c):xs) = do
+			putStr((show a)++" "++(show b)++" "++c++" "++(show (length[((Kord a b c):xs)]))++" ")
+			printKord xs
+		
+printKD (Leaf k) = printKord k
+printKD (Node (Kord x y a1) c1 c2 c3 c4 ) = do
+		putStrLn("Node "++(show x)++" "++(show y)++" "++a1)
+		printKD c1
+		printKD c2
+		printKD c3
+		printKD c4
+--infas x2 a i xs = do
+		
+		--infa b (i+1) xs
+		--b <- infa x a
+		--let a = (Leaf [])
+		--iKD (a) (Kord (read x :: Float) (read x :: Float) (read x :: String))
+		--infa inh
+		--case(parse koord "some text" x) of
+		--Left err -> putStr "Parse error: " >> print err
+		--Right b  -> iKD (a) (b)
+ 		
+
+		
+main::IO () 
+main = do 
+		outh <- openFile "file.out" WriteMode
+		setgor 1 outh
+		--hClose inh
+		hClose outh
+		run
+
+run = do
+		inh <- readFile "file.out"
+		let (x:xs) = lines inh
+		let a = (Leaf [])
+		--print(parseLine x)
+		--print(splitOn " " x)
+		let c = infa (x:xs) a 1
+		printKD c
+		return ()
+
+--operate :: String
+--operate = "aaa"
+
+ 
+
+
+--inttostr :: Int -> String
+--inttostr a = show a
---
+++ Kvad.hs
@@ -0,0 +1,55 @@
+module Kvad where
+import Prelude
+--import Data.Map as Map
+import Data.List(foldl')
+--data Kord =(Float,Float)
+--class Summ a where
+--(+) :: a -> a -> a
+
+
+data Kord = Kord {absc::Float,ord::Float,name::String}
+
+
+(+.) :: Kord -> Kord -> Kord
+(Kord a b a1) +. (Kord c d a2) = (Kord (a + c) (b + d) (a1++a2))
+
+(*.) :: Kord-> Float -> Kord
+(Kord a b a1) *. (c) = (Kord (a*c) (b*c) (a1++"*"++(show c)))
+
+--instance Summ Kord where
+--(Kord a b) + (Kord c d) = (Kord (a + c) (b + d)) 
+ 
+--summaKord (a,b) (c,d) = ((a+c),(b+d))
+summaAllKord a = foldl' (+.) (Kord 0 0 "0") a
+--delenKord (a,b) c = ((a/c),(b/c))
+srednArifm a = (summaAllKord a) *. (1/5)
+
+
+
+data KD a = Leaf [Kord] | Node Kord (KD a) (KD a) (KD a) (KD a) 
+
+
+
+func (Leaf k) = (foldl iKD (Node (srednArifm k) e e e e) k) where e = Leaf []
+--func = undefined
+
+--delen (a,b) c = ((a/c),(b/c))
+--func1 a [x1,x2,x3,x4,x5] = (iKD(iKD(iKD(iKD (iKD a x1) x2) x3) x4) x5)
+--func1 a [] = a
+--func1 a (x:xs) = (func1 (iKD a x) xs) 
+ 
+
+--iKD1 (x:xs) a = (Pair x a):(iKD1 xs a)
+--Up Down Right Left
+
+iKD (Leaf k) (Kord a b c) =  if ((length[k]+1)<3) then (Leaf ((Kord a b c):k)) 
+												   else (func (Leaf ((Kord a b c):k)))
+																   	
+iKD (Node (Kord x y a1) c1 c2 c3 c4 ) (Kord x1 y1 a2) = if ((x1<x)&&(y1>y)) then (Node(Kord x y a1) (iKD c1 (Kord x1 y1 a2)) c2 c3 c4 )
+															 else (if ((x1>x)&&(y1>y)) then (Node(Kord x y a1) c1 (iKD c2 (Kord x1 y1 a2)) c3 c4 )
+																					   else (if ((x1<x)&&(y1<y)) then (Node(Kord x y a1) c1 c2 (iKD c3 (Kord x1 y1 a2))c4 )
+																												 else (Node(Kord x y a1) c1 c2 c3 (iKD c4 (Kord x1 y1 a2))))) 
+
+ 
+
+
